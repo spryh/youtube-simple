fb9e5bb35b4c561e98d2b7966098e85d8ed2e1bc
Spryh <skyphr@gmail.com>
2017-11-13 17:15:55 -0800

    Removed Amazon

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import logo from './logo.svg'
 import './App.css'
 import Youtube from './Youtube'
-import Amazon from './Amazon'
+
 
 class App extends Component {
    render() {
@@ -13,7 +13,6 @@ class App extends Component {
                <h1 className="App-title">Video & Amazon Genie</h1>
             </header>
             <Youtube />
-            <Amazon />
          </div>
       )
    }
