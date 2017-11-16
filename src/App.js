50dedda525cfc5352a75eae402e6cc7913df6b47
Spryh <skyphr@gmail.com>
2017-11-15 20:30:48 -0800

    Removed Amazon, after repo rename

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
          <div className="App">
             <header className="App-header">
                <img src={logo} className="App-logo" alt="logo" />
-               <h1 className="App-title">Video & Amazon Genie</h1>
+               <h1 className="App-title">Youtube Genie</h1>
             </header>
             <Youtube />
          </div>
--- src/Youtube.js
+++ src/Youtube.js
@@ -15,71 +15,73 @@ const obj = 'snippet'
 const yt_embed_url = 'https://www.youtube.com/embed/'
 
 var yt_URL = `${api_yt_srch_url}${api_yt_key_prop}${api_yt_key}&${
-   query_prop
+  query_prop
 }${encodeURI(query)}&${type_prop}${type}&${result_prop}${result}&${obj_prop}${
-   obj
+  obj
 }`
 
 class Youtube extends Component {
-   //construct an array to keep the data
-   constructor(props) {
-      super(props)
-      this.state = {
-         yt_results: []
-      }
-      //bind the array
-      this.clicked = this.clicked.bind(this)
-   }
-   clicked() {
-      //console.log('clicked');
-      fetch(yt_URL)
-         .then(response => response.json())
-         .then(responseJson => {
-            //return responseJson.movies;
-            //console.log(responseJson);
-            //map will loop through the array
-            const yt_results = responseJson.items.map(
-               obj => yt_embed_url + obj.id.videoId
-            )
-            //save it to this state
-            this.setState({ yt_results })
-            console.log(responseJson)
-         })
-         .catch(error => {
-            console.error(error)
-         })
-   }
+  //construct an array to keep the data
+  constructor(props) {
+    super(props)
+    this.state = {
+      yt_results: []
+    }
+    //bind the array
+    this.clicked = this.clicked.bind(this)
+  }
+  clicked() {
+    //console.log('clicked');
+    fetch(yt_URL)
+      .then(response => response.json())
+      .then(responseJson => {
 
-   render() {
-      //log the url to the console
-      /*console.log(finalURL);
+        console.log(responseJson.items)
+
+        //responseJson.items is the array of objects
+        //map will loop through the array
+        const yt_results = responseJson.items.map(
+          obj =>  yt_embed_url + obj.id.videoId
+        )
+       console.log(yt_results)
+        //save it to this state
+        this.setState({ yt_results })
+      })
+      .catch(error => {
+        console.error(error)
+      })
+  }
+
+  render() {
+    //log the url to the console
+    /*console.log(finalURL);
         console.log(this.state.amz_results);
         console.log(encodeURI(query));*/
-      return (
-         <div>
-            <button onClick={this.clicked}>Get {query} Videos</button>
-            {/*rotate through the array
+    return (
+      <div>
+        <button onClick={this.clicked}>Get {query} Videos</button>
+        {/*rotate through the array
                         console.log(link);*/}
-            {this.state.yt_results.map((link, i) => {
-               // div needs a unique key
-               var frame = (
-                  <div className="youtube" key={i}>
-                     <iframe
-                        width="560"
-                        height="315"
-                        title="abc"
-                        src={link}
-                        frameBorder="0"
-                        allowFullScreen
-                     />
-                  </div>
-               )
-               return frame
-            })}
-            {this.frame}
-         </div>
-      )
-   }
+        {this.state.yt_results.map((link, i) => {
+          // div needs a unique key
+          var frame = (
+            <div className="youtube" key={i}>
+              <iframe
+                width="560"
+                height="315"
+                title="abc"
+                src={link}
+                frameBorder="0"
+                allowFullScreen
+              />
+            </div>
+          )
+          return frame
+        })}
+        {this.frame}
+      </div>
+    )
+  }
 }
 
 export default Youtube
